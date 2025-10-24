105e2e2f1281fa8b954843c2309e958434895a60
MedZaim <mohamed.zaim3@usmba.ac.ma>
2025-10-25 00:53:49 +0100

    UI launcher compiles and runs samples (console subsystem); verified corr_ex2_flr,

--- code_graphics/src_graphics/correc_exam/corr_ex2_flr.cpp
+++ code_graphics/src_graphics/correc_exam/corr_ex2_flr.cpp
@@ -1,15 +1,19 @@
-# include "graphics.h"
+# include "../../Graphics/graphics.h"
 # include <cmath>
+
+#ifndef PI
+#define PI 3.14159265358979323846
+#endif
 
 
 using namespace std;
 
 void ellipse_teta(int xc, int yc, int a, int b, double teta) {
     double x, y, x1, y1;
-    for (double i = 0; i < 360; i += 0.003) {
+    for (double i = 0; i < 360; i += 0.009) {
 
-        x = a * cos(i * M_PI);
-        y = b * sin(i * M_PI);
+        x = a * cos(i * PI);
+        y = b * sin(i * PI);
 
         x1 = x * cos(teta) + y * -sin(teta);
         y1 = x * sin(teta) + y * cos(teta);
@@ -21,7 +25,7 @@ void ellipse_teta(int xc, int yc, int a, int b, double teta) {
 void fleur(int xc = 100, int yc = 180, int r = 50, int a = 100, int b = 50) {
     circle(xc, yc, r);
     // Dessiner les ellipses
-    for (double angle = 0; angle < 2 * M_PI; angle += M_PI / 3) {
+    for (double angle = 0; angle < 2 * PI; angle += PI / 3) {
 
         int x = (r + a) * cos(angle);
         int y = (r + a) * sin(angle);
@@ -30,8 +34,8 @@ void fleur(int xc = 100, int yc = 180, int r = 50, int a = 100, int b = 50) {
 }
 
 int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, (char *) " ");
+    // Open a titled window instead of legacy initgraph
+    initwindow(800, 600, "Fleur - Correction Ex2 (ellipses teta)");
     fleur(getmaxx() / 2, getmaxy() / 2,50, 100,50);
     getch();
     closegraph();
