8c31016eb3dcd8b9d0eda175679157203a3ee0a7
MedZaim <mohamed.zaim3@usmba.ac.ma>
2025-10-25 20:36:27 +0100

    improving ui

---
+++ UI_luncher_infographie_projects/flowlayout.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QLayout>
+#include <QRect>
+#include <QStyle>
+#include <QWidget>
+#include <QList>
+
+// Simple FlowLayout for Qt (wraps items like CSS flex/flow)
+// Adapted from Qt examples.
+class FlowLayout : public QLayout
+{
+    Q_OBJECT
+public:
+    explicit FlowLayout(QWidget *parent = nullptr, int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    explicit FlowLayout(int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    ~FlowLayout();
+
+    void addItem(QLayoutItem *item) override;
+    int horizontalSpacing() const;
+    int verticalSpacing() const;
+    Qt::Orientations expandingDirections() const override;
+    bool hasHeightForWidth() const override;
+    int heightForWidth(int) const override;
+    int count() const override;
+    QLayoutItem *itemAt(int index) const override;
+    QSize minimumSize() const override;
+    void setGeometry(const QRect &rect) override;
+    QSize sizeHint() const override;
+    QLayoutItem *takeAt(int index) override;
+
+private:
+    int doLayout(const QRect &rect, bool testOnly) const;
+    int smartSpacing(QStyle::PixelMetric pm) const;
+
+    QList<QLayoutItem *> itemList;
+    int m_hSpace;
+    int m_vSpace;
+};
